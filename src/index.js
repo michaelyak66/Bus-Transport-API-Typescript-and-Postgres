d10a041ebe5968fafb94ad0d177426a95110c363
Mcdavid Emereuwa <mcdavidemereuwa95@gmail.com>
2019-07-02 08:39:00 +0100

    ch(index.js): add PORT environment variable
    - install dotenv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import helmet from 'helmet';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
 import { logger } from './helpers/utils';
 
+dotenv.config();
+
 const app = express();
+const port = parseInt(process.env.PORT, 10) || 1337;
 // Log requests to the console.
 app.use(morgan('dev'));
 
@@ -18,5 +22,5 @@ app.get('/api/v1', (req, res) => res.status(200).send({
   message: 'Welcome Save A Seat API'
 }));
 
-app.listen(1337);
-logger().info('app running on port ', 1337);
+app.listen(port);
+logger().info(`app running on port ${port}`);
